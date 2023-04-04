4f7363f7d5b031f09a79ba96998f2ca6363d00f4
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-04-04 21:36:46 +0300

    Task6 part1

--- Task6.py
+++ Task6.py
@@ -1,1 +1,19 @@
+﻿
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from Task6_models import create_tables
 
+
+login = 'postgres'
+password = 'Maxim0055!!!'
+db_name = 'Task6'
+
+DSN = f'postgresql://{login}:{password}@localhost:5432/{db_name}'
+engine = sqlalchemy.create_engine(DSN)
+
+create_tables(engine)
+
+Session = sessionmaker(bind = engine)
+session = Session()
+
+session.close()
