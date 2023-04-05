7536dd09375eb64cb2adb91ea215b1fb2706f673
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-04-05 23:01:29 +0300

    final ver1

--- Task6.py
+++ Task6.py
@@ -1,7 +1,7 @@
-﻿
+﻿import json
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from Task6_models import create_tables
+from Task6_models import create_tables, Publisher, Book, Shop, Stock, Sale
 
 
 login = 'postgres'
@@ -15,5 +15,31 @@ create_tables(engine)
 
 Session = sessionmaker(bind = engine)
 session = Session()
+
+with open('tests_data.json', 'r', encoding = 'UTF-8') as file:
+    data = json.load(file)
+
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
+session.commit()
+
+print('Введите номер издателя для отчета:')
+for pb in session.query(Publisher).all():
+    print(pb)
+
+choice = int(input())
+
+subq1 = session.query(Publisher).filter(Publisher.id == choice).subquery()
+subq2 = session.query(Book).join(subq1, Book.id_publisher == subq1.c.id).subquery()
+subq3 = session.query(Stock).join(subq2, Stock.id_book == subq2.c.id).subquery()
+for result in session.query(Sale).join(subq3, Sale.id_stock == subq3.c.id).all():
+    print(result)
 
 session.close()
--- Task6_models.py
+++ Task6_models.py
@@ -12,6 +12,9 @@ class Publisher(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=60), unique=True)
 
+    def __str__(self):
+        return f'Издатель {self.id}: {self.name}'
+
 class Book(Base):
     __tablename__ = "book"
 
@@ -21,11 +24,14 @@ class Book(Base):
 
     publisher = relationship(Publisher, backref="book")
 
+    
 class Shop(Base):
     __tablename__ = "shop"
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=60), unique=True)
+
+    
 
 class Stock(Base):
     __tablename__ = "stock"
@@ -38,16 +44,20 @@ class Stock(Base):
     book = relationship(Book, backref="stock")
     shop = relationship(Shop, backref="stock")
 
+    
 class Sale(Base):
     __tablename__ = "sale"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    price = sq.Column(sq.Integer, nullable=False)
+    price = sq.Column(sq.String(length=10), nullable=False)
     date_sale = sq.Column(sq.DateTime, default=dt.datetime.now)
     id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
     stock = relationship(Stock, backref="sale")
+
+    def __str__(self):
+        return f"{self.stock.book.title:<40} | {self.stock.shop.name:<15} | {float(self.price)*self.count:<5} | {self.date_sale.strftime('%Y-%m-%d'):<10}" 
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
